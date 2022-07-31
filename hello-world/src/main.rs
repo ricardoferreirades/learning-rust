f5b265347a62683a9db9dfa7b383468d638f4be5
Ricardo Ferreira <ricardoferreirades@gmail.com>
2022-07-31 15:30:51 +0200

    chore: convert structure to works with cargo
    Signed-off-by: Ricardo Ferreira <ricardoferreirades@gmail.com>

---
+++ hello-world/src/main.rs
@@ -0,0 +1,8 @@
+fn main() { // Defined a function
+    println!("Hello, world!"); // prints a text on screen which calls a rust macro (!)
+} // function body is required
+
+// to compile it $ rustc main.rs
+// to run it $ ./main
+
+// indentaion must be with 4 spaces
